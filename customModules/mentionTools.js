710975b92e6fd2e8e3a2277e180991263623643e
Zachary Mitchell <31785316+zachMitchell@users.noreply.github>
2020-11-01 15:45:33 -0500

    I forgot to turn this off

--- customModules/mentionTools.js
+++ customModules/mentionTools.js
@@ -8,12 +8,12 @@ function replaceMentionTags(msg,text){
     //I'm not sure what ! is, but it happens so it's here:
     var mentionTypes = ['<@','<@!','<@&'];
     var selectedTypeIndex = 0;
-    console.log(text,msg.mentions);
+    // console.log(text,msg.mentions);
     for(var i = 0;i < text.length;i++){
         //Look for an id, if we hit > we then seaarch for that ID in the message object.
         //user.username
         if(text[i] == '<' && text[i+1] == '@'){
-            console.log('hi');
+            // console.log('hi');
             parsingId = true;
             selectedTypeIndex = 0;
             i+=2;
@@ -28,7 +28,7 @@ function replaceMentionTags(msg,text){
                 var targetUser = msg.mentions.users.get(currId) || msg.mentions.roles.get(currId);
                 if(targetUser) result+='@'+(targetUser.username || targetUser.name); //name is for a "role"; apparently bots fall into this category as well.
                 else{
-                    console.log(currId);
+                    // console.log(currId);
                     result+=mentionTypes[selectedTypeIndex]+currId+'>';
                 }
                 currId = '';
